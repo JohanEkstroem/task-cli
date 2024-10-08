7b9ea340671cb6411521d8e7f3b79b0d283dc4e3
JohanEkstroem <johan.ekstrom@live.se>
2024-10-08 20:44:00 +0200

    Add some boilerplate for user input

--- task-cli/Program.cs
+++ task-cli/Program.cs
@@ -1,7 +1,25 @@
-﻿internal class Program
+﻿using System;
+internal class Program
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, Echostreem!");
-    }
+	private static void Main(string[] args)
+	{
+		bool isRunning = true;
+		Console.WriteLine("Hello world!");
+		while (isRunning)
+		{
+			string input = Console.ReadLine();
+
+			switch (input.ToLower())
+			{
+				case "exit":
+					isRunning = false;
+					System.Console.WriteLine("Programmet avslutas");
+					break;
+
+				default:
+					System.Console.WriteLine(input);
+					break;
+			}
+		}
+	}
 }
